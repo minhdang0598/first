21cbce5f795db32a030449be8df6dc54b11ee25b
minhdang0598 <minhdang1234x@gmail.com>
2020-03-18 08:53:49 +0700

    1

--- Todo/views.py
+++ Todo/views.py
@@ -1,8 +1,14 @@
 from django.shortcuts import render, redirect
 from .models import Task
+from rest_framework.generics import (
+    ListCreateAPIView,
+    RetrieveUpdateDestroyAPIView, )
+from rest_framework import viewsets
+from rest_framework.pagination import PageNumberPagination
+from rest_framework.permissions import IsAuthenticated
+from rest_framework import serializers
 
 
-# Create your views here.
 def index(request):
     data = {'Tasks': Task.objects.all().order_by("-id")}
     if request.method == "POST":
@@ -24,3 +30,22 @@ def index(request):
             task.delete()
 
     return render(request, 'pages/home.html', data)
+
+
+class TaskListSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Task
+        fields = '__all__'
+
+
+class TaskDetailUpdateAPIView(viewsets.GenericViewSet,
+                              RetrieveUpdateDestroyAPIView):
+    queryset = Task.objects.all()
+    serializer_class = TaskListSerializer
+    lookup_field = 'id'
+
+
+class TaskListCreateAPIView(viewsets.GenericViewSet,
+                            ListCreateAPIView):
+    serializer_class = TaskListSerializer
+    queryset = Task.objects.all()
